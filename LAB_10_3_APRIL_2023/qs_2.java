70eab2761ae8f4411e5149c2d463ca52bb07ac5f
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-04-09 20:14:35 +0530

    Create qs_2.java

---
+++ LAB_10_3_APRIL_2023/qs_2.java
@@ -0,0 +1,34 @@
+// qs 2 hai bhai 
+import java.util.Scanner;
+
+class Circle {
+    private double radius;
+
+    public void set_radius(double radius) {
+        this.radius = radius;
+    }
+
+    public double get_radius () {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+public class qs_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter radius of circle ");
+        double radius = sc.nextDouble();
+        Circle c = new Circle();
+        c.set_radius(radius);
+        System.out.println("area" + c.getArea());
+        System.out.println("preimeter is " + c.getPerimeter());
+    }
+}
